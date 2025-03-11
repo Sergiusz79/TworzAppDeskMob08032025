11a006993c9b4868a0942c3dfb2dd6bb4d906052
Sergiusz79 <mc.sergey30@gmail.com>
2025-03-11 13:07:29 +0100

    Task2-2

--- Task2.cpp
+++ Task2.cpp
@@ -3,34 +3,40 @@
 
 using namespace std;
 
+void printArray(char *arr, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+};
+
+void sortArray(char *arr, size_t size)
+{
+    int temp;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+};
+
 int main()
 {
     char temp;
     char array[]{"TGP"};
-    int size = strlen(array); //длинна char без учёта 0
-    for (int i = 0; i < size; i++)
-    {
-        cout << array[i] << ' ';
-    }
-    cout<<endl;
+    size_t size = strlen(array); //длинна char без учёта 0
+    printArray(array, size);
     cout << "*********" << endl;
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size-1; j++)
-        {
-            if (array[j] > array[j+1])
-            {
-                temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < size; i++)
-    {
-        cout << array[i] << ' ';
-    }
+    sortArray(array, size);
+    printArray(array, size);
     return 0;
 }
-
-//toascii
