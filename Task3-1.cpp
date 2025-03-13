d30d88587c4f618d8a45021b351cadd6cffae39f
Sergiusz79 <mc.sergey30@gmail.com>
2025-03-13 06:55:00 +0100

    task3-1.1

---
+++ Task3-1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool customSort(char a, char b) {
+    if (isdigit(a) && isdigit(b)) return a < b;
+    if (isupper(a) && isupper(b)) return a < b;
+    if (islower(a) && islower(b)) return a < b;
+
+    if (isdigit(a)) return true;   // Цифры идут первыми
+    if (isdigit(b)) return false;
+
+    if (isupper(a)) return true;   // Затем заглавные буквы
+    if (isupper(b)) return false;
+
+    return false;  // Нижний регистр идёт последним
+}
+
+int main() {
+    string input;
+    cout << "Введите строку: ";
+    getline(cin, input);
+
+    // Удаляем пробелы
+    input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
+
+    sort(input.begin(), input.end(), customSort);
+
+    cout << "Отсортированная строка: " << input << endl;
+    return 0;
+}
--- Task3.cpp
+++ Task3.cpp
@@ -1,35 +1,47 @@
-#include <iostream>               
-#include <cstring>
+#include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-int main()                       
-{
-                                      
-    char temp;
-    char array[]{"TGP"};
-    int size = strlen(array); 
-    for (int i = 0; i < size; i++)
-    {
-        cout << array[i] << ' ';
-    }
-    cout<<endl;
-    cout << "*********" << endl;
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size-1; j++)
-        {
-            if (array[j] > array[j+1])
-            {
-                temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
+// Функция сравнения для сортировки
+bool customCompare(char a, char b) {
+    if (isdigit(a) && isdigit(b)) return a < b;
+    if (isupper(a) && isupper(b)) return a < b;
+    if (islower(a) && islower(b)) return a < b;
+
+    if (isdigit(a)) return true;   // Цифры идут первыми
+    if (isdigit(b)) return false;
+
+    if (isupper(a)) return true;   // Затем заглавные буквы
+    if (isupper(b)) return false;
+
+    return false;  // Нижний регистр идёт последним
+}
+
+// Пузырьковая сортировка
+void bubbleSort(string &s) {
+    int n = s.length();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (!customCompare(s[j], s[j + 1])) {
+                swap(s[j], s[j + 1]);  // Меняем местами элементы, если они в неверном порядке
             }
         }
     }
-    for (int i = 0; i < size; i++)
-    {
-        cout << array[i] << ' ';
-    }
-    return 0;                     
+}
+
+int main() {
+    string input;
+    cout << "Input data: ";
+    getline(cin, input);
+
+    // Удаляем пробелы
+    input.erase(remove_if(input.begin(), input.end(), ::isspace), input.end());
+
+    // Сортируем пузырьковой сортировкой
+    bubbleSort(input);
+
+    cout << "Sorted string: " << input << endl;
+    return 0;
 }
